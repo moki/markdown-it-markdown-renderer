b9fb1879c5994842beb959815da11854aced1da4
moki-codes <moki-codes@yandex-team.ru>
2023-02-09 22:42:27 +0400

    feat(rules/inline): hardbreak

---
+++ src/rules/inline/hardbreak.ts
@@ -0,0 +1,10 @@
+import Renderer from 'markdown-it/lib/renderer';
+
+const hardbreak: Renderer.RenderRuleRecord = {
+    hardbreak: function () {
+        return '\n';
+    },
+};
+
+export {hardbreak};
+export default {hardbreak};
