07fec1c77d1c7a747299d4ee2242301cf4447db1
moki-codes <moki-codes@yandex-team.ru>
2023-02-09 20:43:18 +0400

    fix(renderer): debug mode

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,22 +1,65 @@
 import Renderer from 'markdown-it/lib/renderer';
+import Token from 'markdown-it/lib/token';
+import {Options} from 'markdown-it';
 
 export type MarkdownRendererParams = {
     customRules?: Renderer.RenderRuleRecord;
+    mode?: MarkdownRendererMode;
 };
+
+export enum MarkdownRendererMode {
+    Production,
+    Development,
+}
 
 class MarkdownRenderer extends Renderer {
     static defaultRules: Renderer.RenderRuleRecord = {};
 
+    static ruleWithDebug(name: string, handler: Renderer.RenderRule) {
+        const modeMsg = (argsStr: string) =>
+            `${this.constructor.name}: rule <${name}> called with args <${argsStr}>`;
+
+        return function (
+            this: MarkdownRenderer,
+            ...args: [Token[], number, Options, unknown, Renderer]
+        ) {
+            if (this.mode === MarkdownRendererMode.Development) {
+                console.debug(modeMsg(JSON.stringify(args)));
+            }
+
+            return handler.apply(this, args);
+        };
+    }
+
+    private mode: MarkdownRendererMode;
+
     constructor(params: MarkdownRendererParams = {}) {
         super();
 
-        const {customRules = {}} = params;
+        const {customRules = {}, mode = MarkdownRendererMode.Production} = params;
 
-        const rules = Object.assign({}, MarkdownRenderer.defaultRules, customRules);
+        this.mode = mode;
+        if (this.mode === MarkdownRendererMode.Development) {
+            console.debug(`${this.constructor.name}: instantiated in the debug mode`);
+        }
 
-        for (const [name, handler] of Object.entries(rules)) {
-            if (!handler) {
+        this.setRules({...MarkdownRenderer.defaultRules, ...customRules});
+    }
+
+    setRules(rules: Renderer.RenderRuleRecord) {
+        const setMsg = (name: string, fnStr: string) =>
+            `${this.constructor.name}: could not set rule <${name}> with handler <${fnStr}>`;
+
+        // eslint-disable-next-line prefer-const
+        for (let [name, handler] of Object.entries(rules)) {
+            if (!handler || !name?.length) {
+                console.debug(setMsg(name, JSON.stringify(handler)));
+
                 continue;
+            }
+
+            if (this.mode === MarkdownRendererMode.Development) {
+                handler = MarkdownRenderer.ruleWithDebug(name, handler);
             }
 
             this.rules[name] = handler.bind(this);
