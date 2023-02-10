b805f60d7eef42cdd2e99a77bb691314f9ded28d
moki-codes <moki-codes@yandex-team.ru>
2023-02-10 13:59:37 +0400

    test(rules/inline): code

---
+++ __tests__/rules/inline/code.ts
@@ -0,0 +1,43 @@
+import {MarkdownRenderer} from 'src/renderer';
+import Token from 'markdown-it/lib/token';
+
+import {code} from 'src/rules/inline/code';
+
+describe('code_inline', () => {
+    it('is named code_inline', () => {
+        const handler = code.code_inline;
+
+        expect(handler).toBeTruthy();
+    });
+
+    it('return content wrapped in "`" markup for code_inline token', () => {
+        const renderer = new MarkdownRenderer();
+
+        let handler = code.code_inline;
+        if (!handler) {
+            return;
+        }
+
+        handler = handler.bind(renderer);
+
+        const markup = '```';
+        const content = 'console.log("text");';
+        const expected = markup + content + markup;
+        const tokens = [{type: 'code_inline', markup, content}];
+        const actual = handler(tokens as Token[], 0, {}, {}, renderer);
+
+        expect(actual).toBe(expected);
+    });
+
+    it('render content wrapped in "`" markup for code_inline token', () => {
+        const renderer = new MarkdownRenderer({customRules: {...code}});
+
+        const markup = '```';
+        const content = 'console.log("text");';
+        const expected = markup + content + markup;
+        const tokens = [{type: 'code_inline', markup, content}];
+        const actual = renderer.render(tokens as Token[], {}, {});
+
+        expect(actual).toEqual(expected);
+    });
+});
