a94831ed7efaef5e1cf498f6c52f666647216230
moki-codes <moki-codes@yandex-team.ru>
2023-02-27 03:45:54 +0400

    feat(rules/block): setex headings

--- src/rules/block/heading.ts
+++ src/rules/block/heading.ts
@@ -1,7 +1,7 @@
 import Renderer from 'markdown-it/lib/renderer';
 import Token from 'markdown-it/lib/token';
 
-import {MarkdownRenderer} from 'src/renderer';
+import {MarkdownRenderer, MarkdownRendererEnv} from 'src/renderer';
 
 const heading: Renderer.RenderRuleRecord = {
     heading_open: function (this: MarkdownRenderer, tokens: Token[], i: number) {
@@ -13,14 +13,47 @@ const heading: Renderer.RenderRuleRecord = {
             rendered += this.EOL;
         }
 
-        rendered += markup + this.SPACE;
+        // handle atx headings
+        if (!isSetexHeading(tokens[i])) {
+            rendered += markup + this.SPACE;
+
+            return rendered;
+        }
+
+        this.pending.push(tokens[i]);
+
+        const previous = tokens[i - 1];
+        if (previous?.type === 'paragraph_close') {
+            rendered += this.EOL;
+        }
 
         return rendered;
     },
-    heading_close: function () {
-        return '';
+    heading_close: function (this: MarkdownRenderer, ...args) {
+        const {
+            3: {source},
+        }: {3: MarkdownRendererEnv} = args;
+
+        // no mappings avaialbe for the heading_open
+        // or markdown source wasn't provided via environment
+        const open = this.pending.pop();
+        if (!open?.map || !source?.length) {
+            return '';
+        }
+
+        const [_, end] = open.map;
+
+        const markup = source.slice(end - 1, end);
+
+        return this.EOL + markup.pop();
     },
 };
+
+function isSetexHeading(token: Token) {
+    const {markup} = token;
+
+    return markup.indexOf('=') !== -1 || markup.indexOf('-') !== -1;
+}
 
 export {heading};
 export default {heading};
