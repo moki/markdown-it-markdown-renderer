8646a4d700fca6842037de7af3fddbfa91e7c59e
moki-codes <moki-codes@yandex-team.ru>
2023-03-20 03:00:24 +0400

    test(rules): semantics markdown snapshot
    introduce snapshots tests for cases where we give up to achive
    markdown zero diff and settle for preserving semantics
    (i.e achieving same html render)

--- __tests__/rules/markdown-zero-diff.ts
+++ __tests__/rules/markdown-zero-diff.ts
@@ -49,6 +49,7 @@ const sectionsKeep = new Set([
     'Indented code blocks',
     'Block quotes',
     'List items',
+    'Lists',
 ]);
 
 export const semanticsTests = new Set([
@@ -184,7 +185,8 @@ export const semanticsTests = new Set([
 
     // 'List items'
     253, 254, 255, 256, 258, 259, 260, 262, 263, 268, 272, 273, 274, 275, 276, 277, 279, 280, 282,
-    286, 287, 288, 290, 291, 293, 299, 306, 307, 308, 309, 312, 314, 315, 316, 317, 319, 325, 326,
+    286, 287, 288, 290, 291, 293, 299, 306, 307, 308, 309, 311, 313, 312, 314, 315, 316, 317, 319,
+    320, 324, 325, 326,
 ]);
 
 const units = tests.filter(({section, number}) => {
@@ -209,6 +211,9 @@ const units = tests.filter(({section, number}) => {
 // omit first and last empty lines
 const filterOutEmptyFstLst = (line, i, ls) => (i && i + 1 !== ls.length) || line.trim().length;
 const normalizeMD = (str: string) => str.split('\n').filter(filterOutEmptyFstLst).join('\n');
+
+console.info = (a) => a;
+console.log = (a) => a;
 
 describe('markdown zero diff', () => {
     units.forEach((entry: CommonMarkSpecEntry) => {
