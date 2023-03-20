b00b95befd27b2439cd40796517162ef5f4b2a67
moki-codes <moki-codes@yandex-team.ru>
2023-03-20 17:18:45 +0400

    feat(plugin): introduce plugin interface
    Markdown IT plugin interface
    
    allows to configure MarkdownIt instance to render into markdown
    just by adding the plugin
    
    Closes: #34

---
+++ src/plugin/index.ts
@@ -0,0 +1,27 @@
+import type MarkdownIt from 'markdown-it';
+import {MarkdownRenderer, MarkdownRendererParams} from 'src/renderer';
+
+// import type {PluginWithOptions} from 'markdown-it';
+
+// helpers
+// always evaluate to provided value <v>
+const always = (v: boolean) => () => v;
+// always return input value <a> as a result
+const id = (a: string) => a;
+
+function mdRenderer(parser: MarkdownIt, parameters?: MarkdownRendererParams) {
+    // disable escape rule
+    parser.inline.ruler.at('escape', always(false));
+    // disable entity rule
+    parser.inline.ruler.at('entity', always(false));
+    // disable links normalization
+    parser.normalizeLink = id;
+
+    const renderer = new MarkdownRenderer(parameters);
+
+    // @ts-ignore
+    parser.renderer = renderer;
+}
+
+export {mdRenderer};
+export default {mdRenderer};
